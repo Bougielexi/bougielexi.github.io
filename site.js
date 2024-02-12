adfb071f8a061734fa5df5a84b0650612ed7aba3
Lexi Bougie <300028074@fvtc.edu>
2024-02-12 16:38:23 -0600

    Added welcome message

---
+++ site.js
@@ -0,0 +1,20 @@
+const hours = new Date().getHours() // get the current hour
+
+const isMorning = hours >= 4 && hours < 12 // is it morning?
+const isAfternoon = hours >= 12 && hours < 17 // is it afternoon?
+const isEvening = hours >= 17 || hours < 4 // is it evening?
+
+const welcome = document.querySelector('#welcome')
+const h1 = welcome.querySelector('h1')
+
+// Not sure if it's just my computer but it's taking awhile when I adjust the time for my site to update the welcome message
+
+if (isMorning == true) {
+    h1.textContent = "Good morning! Welcome to Lexi's Page!"
+}
+if (isAfternoon == true) {
+    h1.textContent = "Good afternoon! Welcome to Lexi's Page!"
+}
+if (isEvening == true) {
+    h1.textContent = "Good evening! Welcome to Lexi's Page!"
+}
